72aa1492d8887bb4e4b157f9abc8ea62c3e99e89
Yevgeniy Sobolev <yevgeniyerot@gmail.com>
2019-02-01 22:27:08 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 function removeElementFromBeginningOfArray(array){
   return array .slice(1)
 }
+function removeElementFromEndOfArray(array){
+  
+}
